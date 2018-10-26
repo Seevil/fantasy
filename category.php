5bf541d42785b0f4da338f19efcfb18acf263ef1
Seevil <seevil@outlook.com>
2018-10-26 09:40:59 +0800

    Revert "修复分类页列表显示"
    This reverts commit 27142b1c2e7e6426d3c9063a89d24292ff2448ab.

--- category.php
+++ category.php
@@ -7,7 +7,7 @@ $this->need('header.php');
                     <h1><?php $this->category(',', false); ?></h1>
                     <div class="meta"><?php echo $this->getDescription(); ?></div>
                     <div class="info"></div>
-                    <ul class="archived-posts">
+                    <ul>
 					<?php while($this->next()): ?>
                         <li>
                             <time datetime="<?php $this->date('Y.m.j'); ?>"><?php $this->date('Y.m.j'); ?></time>
